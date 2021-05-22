bab3f97cf121905721ece5604b253d800c309244
Keshav Reddy <keshavlingala@gmail.com>
2021-05-22 13:12:13 +0530

    AWS Credentials fix

--- src/main/java/dev/comakeit/potholechallenge/config/SecurityConfiguration.java
+++ src/main/java/dev/comakeit/potholechallenge/config/SecurityConfiguration.java
@@ -49,8 +49,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requiresChannel().anyRequest().requiresSecure()
-                .and()
                 .cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/api/admin/**").hasAnyAuthority("ADMIN")
                 .antMatchers("/api/contractor/**").hasAnyAuthority("ADMIN", "CONTRACTOR")
--- src/main/java/dev/comakeit/potholechallenge/config/StorageConfig.java
+++ src/main/java/dev/comakeit/potholechallenge/config/StorageConfig.java
@@ -1,13 +1,14 @@
 package dev.comakeit.potholechallenge.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class StorageConfig {
@@ -19,11 +20,31 @@ public class StorageConfig {
     private String region;
 
     @Bean
+    AWSStaticCredentialsProvider getAwsCredentials() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+    }
+
+    @Bean
+    public AmazonS3 getAmazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentialsProvider awsStaticCredentialsProvider = new AWSCredentialsProviderChain(new PropertiesFileCredentialsProvider("AwsCredentials.properties"));
+        return AmazonS3Client.builder()
+                .withCredentials(awsStaticCredentialsProvider)
+                .build();
+    }
+
+    @Bean
     public AmazonS3Client getS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region).build();
+    }
+
+    @PostConstruct
+    public void initAwsCredentials() {
+        System.setProperty("aws.accessKeyId", accessKey);
+        System.setProperty("aws.secretKey", secretKey);
     }
 
 }
